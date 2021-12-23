b44f8e9eb3e85b266272e6e612457329d7155bf1
Rob Gilham <robert.gilham@linkpizza.com>
2021-12-23 03:29:40 +0000

    moved http listener into autoconfig

--- tlsconfig/autotls.go
+++ tlsconfig/autotls.go
@@ -16,6 +16,16 @@ func (at AutoTLS) Config() *tls.Config {
 	return at.certManager.TLSConfig()
 }
 
+func (at AutoTLS) listenHTML() {
+	// listen for lets encrypt inbound
+	log.Println("starting http listener")
+	defer log.Println("closing http listener")
+	proxyHandler := &testhandler{proxy: at.certManager.HTTPHandler(nil)}
+	if err := http.ListenAndServe(":http", proxyHandler); err != nil {
+		log.Printf("Error in http listener  %w", err)
+	}
+}
+
 func newAutoTLS(certDir, domain string) *AutoTLS {
 	wwwDomain := strings.Join([]string{"www", domain}, ".")
 	cm := autocert.Manager{
@@ -23,17 +33,10 @@ func newAutoTLS(certDir, domain string) *AutoTLS {
 		HostPolicy: autocert.HostWhitelist(domain, wwwDomain),
 		Cache:      autocert.DirCache(certDir), //Folder for storing certificates
 	}
+	at := &AutoTLS{certManager: cm}
+	go at.listenHTML()
 
-	// listen for lets encrypt inbound
-	go func() {
-		log.Println("starting http listener")
-		defer log.Println("closing http listener")
-		proxyHandler := &testhandler{proxy: cm.HTTPHandler(nil)}
-		if err := http.ListenAndServe(":http", proxyHandler); err != nil {
-			log.Printf("Error in http listener  %w", err)
-		}
-	}()
-	return &AutoTLS{certManager: cm}
+	return at
 
 }
 
